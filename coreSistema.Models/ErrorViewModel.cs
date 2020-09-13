92910b9f72ab5b2a1be90a55d92c22c9a350bb55
ardzrayo <angelrayo@outlook.com>
2020-09-13 12:27:06 -0500

    solucion organizada en tres capas (web,accesodatos,modelos,utilidades) en blanco

---
+++ coreSistema.Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace coreSistema.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
